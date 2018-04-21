0504ce35bc8f473909dc2665eee458e30c0c13ec
Xiaoru Li <dev@xiaoru.li>
2018-04-21 11:41:58 +0200

    Use NCR version of google images and mkdir shots

--- google-images.js
+++ google-images.js
@@ -1,10 +1,12 @@
 const puppeteer = require('puppeteer')
+const mkdirp = require('mkdirp')
 
+mkdirp('shots')
 ;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto('https://images.google.com')
-  await page.screenshot({path: 'shot01.png'})
+  await page.goto('https://images.google.com/ncr')
+  await page.screenshot({ path: 'shots/shot01.png' })
 
   await browser.close()
 })()
