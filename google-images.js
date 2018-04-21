64d94ce9a82fd07801d63ce9b5501989eeab226f
Xiaoru Li <dev@xiaoru.li>
2018-04-21 12:04:46 +0200

    Refactor code, use device emulation

--- google-images.js
+++ google-images.js
@@ -1,12 +1,19 @@
 const puppeteer = require('puppeteer')
 const mkdirp = require('mkdirp')
+const devices = require('puppeteer/DeviceDescriptors')
 
 mkdirp('shots')
-;(async () => {
+async function run() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
+  await page.emulate(devices['iPad Mini landscape'])
+
   await page.goto('https://images.google.com/ncr')
+  await console.log(page.url())
   await page.screenshot({ path: 'shots/shot01.png' })
 
+
   await browser.close()
-})()
+}
+
+run()
