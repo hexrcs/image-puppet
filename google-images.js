cc559ec23f85061003c27e641fb174cc8af3b3ab
Xiaoru Li <dev@xiaoru.li>
2018-04-22 19:03:18 +0200

    Refactor code to allow passing in options

--- google-images.js
+++ google-images.js
@@ -8,39 +8,50 @@ const fileType = require('file-type')
 const SEARCH_FIELD_HOME = 'input[title="Search"][type="text"]'
 const SEARCH_BUTTON_HOME = 'button[value="Search"]'
 
-const SEARCH_KEYWORD = 'using ipad'
-const DATASET_NAME = 'ipad'
-const DATASET_DIR = `images/${DATASET_NAME}`
+// mkdirp('debugging/screenshots')
 
-mkdirp('debugging/screenshots')
-mkdirp(DATASET_DIR)
+const defaultOptions = {
+  searchKeyword: 'using ipad',
+  downloadDir: 'data',
+  datasetName: 'ipad',
+  maxCount: 100,
+  maxWidth: 0,
+  maxHeight: 0,
+  minWidth: 0,
+  minHeight: 0,
+  convertTo: null
+}
 
-async function run() {
+async function run(options = defaultOptions) {
+  const fullDownloadDir = `${options.downloadDir}/${options.datasetName}`
+  mkdirp(fullDownloadDir)
+
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   await page.setViewport({ width: 1000, height: 600 })
 
-  await launchSearch(page)
+  await launchSearch(page, options.searchKeyword)
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
 
   await autoScroll(page)
 
-  const imgMetaList = await page.evaluate(ibGetMetaList, SEARCH_KEYWORD)
+  const imgMetaListFull = await page.evaluate(ibGetMetaList, options.searchKeyword)
+  const imgMetaList = imgMetaListFull.slice(0, options.maxCount)
 
   console.log(imgMetaList)
 
-  imgMetaList.forEach(downloadImage)
+  imgMetaList.forEach(downloadImageTo(options.downloadDir))
 
   // await browser.close()
 }
 
 run()
 
-async function launchSearch(page) {
+async function launchSearch(page, searchKeyword) {
   await page.goto('https://images.google.com/ncr')
 
   await page.click(SEARCH_FIELD_HOME)
-  await page.keyboard.type(SEARCH_KEYWORD)
+  await page.keyboard.type(searchKeyword)
   await page.click(SEARCH_BUTTON_HOME)
 }
 
@@ -51,32 +62,36 @@ function ibGetMetaList(searchKeyword) {
     e => e['alt'] == `Image result for ${searchKeyword}`
   )
 
-  const imgMetaList = resultImgTags.filter(e => e['name']).map(e => {
-    const parsed = JSON.parse(e.parentElement.nextElementSibling['innerText'])
-    return {
-      oUrl: parsed.ou, // original url
-      pageTitle: parsed.pt,
-      oHeight: parsed.oh,
-      oWidth: parsed.ow,
-      id: parsed.id.slice(0, -1), // trim last colon char
-      ext: parsed.ity
-    }
-  })
+  const imgMetaList = resultImgTags
+    .filter(e => e['name']) // get rid of invalid img tags
+    .map(e => {
+      const parsed = JSON.parse(e.parentElement.nextElementSibling['innerText'])
+      return {
+        url: parsed.ou, // original url
+        pageTitle: parsed.pt,
+        height: parsed.oh,
+        width: parsed.ow,
+        id: parsed.id.slice(0, -1), // trim last colon char
+        ext: parsed.ity
+      }
+    })
   return imgMetaList
 }
 
-async function downloadImage(e) {
-  const fileName = `${e.oWidth}x${e.oHeight}-${e.id}`
-  await download(e.oUrl, DATASET_DIR, {
-    filename: fileName
-  })
-  const buffer = readChunk.sync(`${DATASET_DIR}/${fileName}`, 0, 4100)
-  const fileExt = fileType(buffer).ext
-  fs.rename(
-    `${DATASET_DIR}/${fileName}`,
-    `${DATASET_DIR}/${fileName}.${fileExt}`,
-    err => (err ? console.log(err) : null)
-  )
+function downloadImageTo(datasetDir) {
+  return async function(e) {
+    const fileName = `${e.width}x${e.height}-${e.id}`
+    await download(e.url, datasetDir, {
+      filename: fileName
+    })
+    const buffer = readChunk.sync(`${datasetDir}/${fileName}`, 0, 4100)
+    const fileExt = fileType(buffer).ext
+    fs.rename(
+      `${datasetDir}/${fileName}`,
+      `${datasetDir}/${fileName}.${fileExt}`,
+      err => (err ? console.log(err) : null)
+    )
+  }
 }
 
 // stolen from https://github.com/GoogleChrome/puppeteer/issues/844
